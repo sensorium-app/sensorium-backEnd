c0512ad4a68455f7f2602902f92b9e6609d7a5c5
Leonardo Gutiérrez Hernández <leo342135@gmail.com>
2018-06-22 08:37:07 -0500

    Phase 1: Basic clustering

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sensorium
-App to connect with people with who you share a lot in common
+App's Logic to connect with people with who you share a lot in common
+
+It's a firebase cloud functions project for clustering and psycellium magic!
+
+License pending
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,206 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import * as moment from 'moment';
 
-admin.initializeApp(functions.config().firebase);
+admin.initializeApp();
 const db = admin.firestore();
 
 export const clusterCreation = functions.firestore
   .document('sensates/{id}')
-  .onCreate(event => {
+  .onCreate((snap, context) => {
 
-    console.log(event.params.id);
+    const newSensateId = context.params.id;
+    console.log(newSensateId);
 
     return new Promise((resolve, reject) =>{
-        const sensate = event.data.data();
+        const sensate = snap.data();
         console.log('sensateData',sensate);
 
+        const sensateDesiredClusters = sensate.desiredClusters;
+        //const sensatesLanguages = sensate.languagesSpoken;
         const sensatesDoB = sensate.dateTimeOfBirth;
-        const sensatesLanguages = sensate.languagesSpoken;
+        const sensatesSkills = sensate.skills;
         const sensatesHobbies = sensate.hobbies;
         const sensatesInterests = sensate.interests;
-        const sensatesSkills = sensate.skills;
-        const sensatesShowsCharacter = sensate.showsCharacter;
+        //const sensatesShowsCharacter = sensate.showsCharacter;
 
-        db.collection('clusters').get()//filter here
-        .then((snapshot) => {
+        //Fix this query aggregation issue
+        //let clustersRef = db.collection('clusters');
+        let clustersRef;
 
-            const clusters = snapshot;
+        //Aggregate the query
+        if(sensateDesiredClusters['dateTimeOfBirth']){
+            let dob = moment(sensatesDoB);
+            
+            console.log(extractDateFromDate(dob));
 
-            clusters.forEach((cluster) => {
-                const clusterType = cluster.data().type;
-                const clusterTypeData = cluster.data().typeData;
-                
-                //Apply rules for clusterCreation
+            clustersRef = db.collection('clusters')
+                .where('type','==','dateTimeOfBirth')
+                .where('typeData', '==', extractDateFromDate(dob));
+        }
+        if(sensateDesiredClusters['monthAndDay']){
+            let dob = moment(sensatesDoB);
+            //Fix this query aggregation issue
+            /*clustersRef
+                .where('type','==','monthAndDay')
+                .where('typeData', '==',  extractMonthAndDayFromDate(dob));*/
+        }
+        if(sensateDesiredClusters['monthAndYear']){
+            let dob = moment(sensatesDoB);
+            //Fix this query aggregation issue
+            /*clustersRef
+                .where('type','==','monthAndYear')
+                .where('typeData', '==',  extractMonthAndYearFromDate(dob));*/ 
+        }
+        if(sensateDesiredClusters['month']){
+            let dob = moment(sensatesDoB);
+            //Fix this query aggregation issue
+            /*clustersRef
+                .where('type','==','month')
+                .where('typeData', '==', extractMonthFromDate(dob));*/   
+        }
+        
+        clustersRef.get().then((clustersFiltered) => {
 
-            });
+            console.log('then de clusters - 59', clustersFiltered.size, JSON.stringify(clustersFiltered.query));
 
+            if(!clustersFiltered.empty){
+                console.log(clustersFiltered);
+                //add sensate to clusters
+                clustersFiltered.forEach((cluster:any) => {
+                    const clusterData = cluster.data();
+                    console.log(clusterData);
+                    const clusterType = clusterData.type;
+                    
+                    let sensates = clusterData.sensates;
+                    sensates[newSensateId] = true;
+
+                    updateCluster(cluster.id, clusterType, sensates).then((res)=>{
+                        resolve(res);  
+                    }).catch((err)=>{
+                        reject(err);
+                    });
+
+                });
+                console.log('Ok, sensate added');
+                resolve('Ok, sensate added');
+            }else{
+                console.log('add cluster 1');
+                createCluster(newSensateId, sensate).then((res)=>{
+                    resolve(res);
+                }).catch((err)=>{
+                    console.log(err);
+                    reject(err);
+                });
+            }
+
+        }).catch((err)=>{
+            console.log(err);
+            reject(err);
         });
 
     });
 
 });
+
+function updateCluster(clusterId, clusterType, sensates){
+    
+    return new Promise((resolve, reject) =>{
+
+        console.log('update', clusterType);
+
+        db.collection('clusters').doc(clusterId).update({sensates: sensates}).then((sensateAddedResponse:any)=>{
+            console.log('Added to '+clusterType+' cluster', sensateAddedResponse);
+            resolve('Added to '+clusterType+' cluster');
+        }).catch((err)=>{
+            console.log(err);
+            reject(err);
+        });
+    });
+}
+
+function createCluster(newSensateId, sensateData){
+
+    console.log('addCluster begins');
+    let sensateInCluster = {};
+    sensateInCluster[newSensateId] = true;
+
+    return new Promise((resolve, reject) =>{
+        //add clusters
+        const sensateDesiredClusters = sensateData.desiredClusters;
+
+        console.log(sensateDesiredClusters);
+
+        Object.keys(sensateDesiredClusters).forEach((desiredCluster)=>{
+
+            console.log(desiredCluster);
+
+            if(sensateDesiredClusters[desiredCluster] == true){
+                let clusterTypeData;
+
+                if(desiredCluster === 'dateTimeOfBirth'){
+                    console.log('add dateTimeOfBirth');
+                    console.log(sensateData.dateTimeOfBirth);
+                    clusterTypeData = extractDateFromDate(sensateData.dateTimeOfBirth);
+                }
+                if(desiredCluster === 'monthAndDay'){
+                    console.log('add monthAndDay');
+                    clusterTypeData = extractMonthAndDayFromDate(sensateData.dateTimeOfBirth);
+                }
+                if(desiredCluster === 'monthAndYear'){
+                    console.log('add monthAndYear');
+                    clusterTypeData = extractMonthAndYearFromDate(sensateData.dateTimeOfBirth);
+                }
+                if(desiredCluster === 'month'){
+                    console.log('add month');
+                    clusterTypeData = extractMonthFromDate(sensateData.dateTimeOfBirth);
+                }
+
+                const newClusterType = {
+                    id: new Date().getTime(),
+                    name: '',
+                    type: desiredCluster,
+                    typeData: clusterTypeData,
+                    sensates: sensateInCluster,
+                    posts: {},
+                    creation: new Date(),
+                    languages: sensateData.languagesSpoken
+                };
+
+                console.log('newClusterType', newClusterType);
+
+                db.collection('clusters').add(newClusterType).then((responseAdd)=>{
+                    console.log('Cluster created', responseAdd);
+                    resolve('Cluster created');
+                }).catch((err)=>{
+                    console.log(err);
+                    reject(err);
+                });
+            }
+            else{
+                console.log('ignore', desiredCluster);
+            }
+        });
+        
+    });
+}
+
+function extractDateFromDate(date){
+    let dateToProcess = moment(date);
+    return dateToProcess.year() + '-' + dateToProcess.month() + '-' + dateToProcess.date();
+}
+
+function extractMonthAndDayFromDate(date){
+    let dateToProcess = moment(date);
+    return dateToProcess.month() + '-' + dateToProcess.date();
+}
+
+function extractMonthAndYearFromDate(date){
+    let dateToProcess = moment(date);
+    return dateToProcess.year() + '-' + dateToProcess.month();
+}
+
+function extractMonthFromDate(date){
+    let dateToProcess = moment(date);
+    return dateToProcess.month();
+}
